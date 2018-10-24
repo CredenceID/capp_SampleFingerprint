e333cc818e179538effdd8664318e63794972f04
Avi Varshney <avarshney65@gmail.com>
2018-10-24 12:45:11 -0700

    almost done

---
+++ app/src/main/java/com/cid/sample/fingerprint/DeviceFamily.java
@@ -0,0 +1,10 @@
+package com.cid.sample.fingerprint;
+
+public enum DeviceFamily {
+	INVALID,
+	TWIZZLER,
+	TRIDENT,
+	CONE,
+	CTWO,
+	CTAB
+}
---
+++ app/src/main/java/com/cid/sample/fingerprint/FingerprintActivity.java
@@ -0,0 +1,390 @@
+package com.cid.sample.fingerprint;
+
+import android.annotation.SuppressLint;
+import android.app.Activity;
+import android.graphics.Bitmap;
+import android.os.Bundle;
+import android.os.SystemClock;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.credenceid.biometrics.Biometrics;
+import com.credenceid.biometrics.Biometrics.CloseReasonCode;
+import com.credenceid.biometrics.Biometrics.FingerprintReaderStatusListener;
+import com.credenceid.biometrics.Biometrics.OnFingerprintGrabbedListener;
+import com.credenceid.biometrics.Biometrics.OnFingerprintGrabbedWSQListener;
+import com.credenceid.biometrics.Biometrics.ResultCode;
+
+import static com.cid.sample.fingerprint.LaunchActivity.mBiometricsManager;
+import static com.credenceid.biometrics.Biometrics.FmdFormat.ISO_19794_2_2005;
+import static com.credenceid.biometrics.Biometrics.ResultCode.FAIL;
+import static com.credenceid.biometrics.Biometrics.ResultCode.INTERMEDIATE;
+import static com.credenceid.biometrics.Biometrics.ResultCode.OK;
+
+public class FingerprintActivity
+		extends Activity {
+	private final static String mTAG = FingerprintActivity.class.getSimpleName();
+
+	private final Biometrics.ScanType mScanTypes[] = {
+			Biometrics.ScanType.SINGLE_FINGER,
+			// These three scan types are only supported on Trident family of devices.
+			Biometrics.ScanType.TWO_FINGERS,
+			Biometrics.ScanType.ROLL_SINGLE_FINGER,
+			Biometrics.ScanType.TWO_FINGERS_SPLIT
+	};
+
+	private ImageView mFingerprintOneImageView;
+	private ImageView mFingerprintTwoImageView;
+	private Button mOpenCloseButton;
+	private Button mCaptureButton;
+	private Button mMatchButton;
+	private TextView mStatusTextView;
+	private TextView mInfoTextView;
+
+	// If true, then "mOpenClose" button text is "Open" meaning we need to open fingerprint.
+	// If false, then "mOpenClose" button text is "Close" meaning we need to cloes fingerprint.
+	private boolean mOpenFingerprint = true;
+	// We are capturing two fingerprints. If true then capture saves data as first fingerprint; if
+	// false capture saves data as second fingerprint.
+	private boolean mCaptureFingerprintOne = true;
+	// Stores FMD templates (used for fingerprint matching) for each fingerprint.
+	private byte[] mFingerprintOneFMDTemplate = null;
+	private byte[] mFingerprintTwoFMDTemplate = null;
+
+	private FingerprintReaderStatusListener mFingerprintOpenCloseListener =
+			new FingerprintReaderStatusListener() {
+				@Override
+				public void onOpenFingerprintReader(ResultCode resultCode,
+													String hint) {
+					Log.d(mTAG, "onOpenFingerprintReader(): " + resultCode.name());
+
+					// If hint is valid, display it.
+					if (hint != null && !hint.isEmpty())
+						mStatusTextView.setText(hint);
+
+					// This code is returned while sensor is in middle of opening.
+					// Allow user to open sensor is if it failed to open.
+					// Alow user to close sensor if it successfully opened.
+					if (resultCode != INTERMEDIATE)
+						mOpenCloseButton.setEnabled(true);
+
+					if (resultCode == Biometrics.ResultCode.OK) {
+						// Now that sensor is open, if user presses "mOpenCloseButton" fingerprint
+						// sensor should now close.
+						mOpenFingerprint = false;
+
+						// Only if fingerprint opened do we now allow user to capture fingerprints.
+						mCaptureButton.setEnabled(true);
+						// If fingerprint opened then we change button to say "Close".
+						mOpenCloseButton.setText(getString(R.string.close));
+					}
+				}
+
+				@SuppressLint("SetTextI18n")
+				@Override
+				public void onCloseFingerprintReader(ResultCode resultCode,
+													 CloseReasonCode closeReasonCode) {
+					if (resultCode == Biometrics.ResultCode.OK)
+						mStatusTextView.setText("Fingerprint Closed: " + closeReasonCode.name());
+
+					mOpenFingerprint = true;
+
+					// Change text back to "Open" and allow button to be clickable.
+					mOpenCloseButton.setText(getString(R.string.open));
+					mOpenCloseButton.setEnabled(true);
+					// Since sensor is closed, user should NOT be able to press capture or match.
+					mCaptureButton.setEnabled(false);
+					mMatchButton.setEnabled(false);
+				}
+			};
+
+
+	// --------------------------------------------------------------------------------------------
+	//
+	// Methods called on Activity lifecycle.
+	//
+	// --------------------------------------------------------------------------------------------
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_fingerprint);
+		Log.d(mTAG, "onCreate(Bundle)");
+
+		this.initializeLayoutComponents();
+		this.configureLayoutComponents();
+	}
+
+	/* Invoked when user pressed back menu button. */
+	@Override
+	public void onBackPressed() {
+		super.onBackPressed();
+		Log.d(mTAG, "onBackPressed()");
+
+		// Tell biometrics to cancel current on-going capture.
+		mBiometricsManager.cancelCapture();
+		// Close all open peripherals.
+		mBiometricsManager.closeFingerprintReader();
+
+		// If user presses back button then they are exiting application. If this is the case then
+		// tell C-Service to unbind from this application.
+		mBiometricsManager.finalizeBiometrics(false);
+	}
+
+	/* Invoked when application is killed, either by user or system. */
+	@Override
+	protected void onDestroy() {
+		super.onDestroy();
+
+		// Tell biometrics to cancel current on-going capture.
+		mBiometricsManager.cancelCapture();
+		// Close all open peripherals.
+		mBiometricsManager.closeFingerprintReader();
+
+		// If user presses back button then they are exiting application. If this is the case then
+		// tell C-Service to unbind from this application.
+		mBiometricsManager.finalizeBiometrics(false);
+	}
+
+	// --------------------------------------------------------------------------------------------
+	//
+	// Methods used for initialization of layout components.
+	//
+	// --------------------------------------------------------------------------------------------
+	private void
+	initializeLayoutComponents() {
+		mFingerprintOneImageView = findViewById(R.id.finger_one_imageview);
+		mFingerprintTwoImageView = findViewById(R.id.finger_two_imageview);
+
+		mOpenCloseButton = findViewById(R.id.open_close_button);
+		mCaptureButton = findViewById(R.id.capture_button);
+		mMatchButton = findViewById(R.id.match_button);
+
+		mStatusTextView = findViewById(R.id.status_textview);
+		mInfoTextView = findViewById(R.id.fingeprint_info_textview);
+	}
+
+	private void
+	configureLayoutComponents() {
+		// Only allow capture once fingerprint is open.
+		// Only allow match once both fingerprints have been captured.
+		this.setCaptureMatchButtonEnable(false);
+
+		mFingerprintOneImageView.setOnClickListener((View v) -> {
+			mFingerprintOneImageView.setBackground(getResources().getDrawable(R.drawable.image_border_on));
+			mFingerprintTwoImageView.setBackground(getResources().getDrawable(R.drawable.image_border_off));
+
+			mCaptureFingerprintOne = true;
+		});
+
+		mFingerprintTwoImageView.setOnClickListener((View v) -> {
+			mFingerprintOneImageView.setBackground(getResources().getDrawable(R.drawable.image_border_off));
+			mFingerprintTwoImageView.setBackground(getResources().getDrawable(R.drawable.image_border_on));
+
+			mCaptureFingerprintOne = false;
+		});
+
+		mOpenCloseButton.setOnClickListener((View v) -> {
+			// Disable button so user does not try a second open while fingerprint it opening.
+			// Hide capture/math buttons since sensor is opening/closing.
+			this.setAllButtonEnable(false);
+
+			if (mOpenFingerprint)
+				mBiometricsManager.openFingerprintReader(mFingerprintOpenCloseListener);
+			else mBiometricsManager.closeFingerprintReader();
+		});
+
+		mCaptureButton.setOnClickListener((View v) -> {
+			mOpenCloseButton.setEnabled(false);
+			mCaptureButton.setEnabled(false);
+			mInfoTextView.setText("");
+
+			if (mCaptureFingerprintOne)
+				this.captureFingerprintOne();
+			else this.captureFingerprintTwo();
+		});
+
+		mMatchButton.setOnClickListener((View v) -> {
+			this.setAllButtonEnable(false);
+
+			this.matchFMDTemplates(mFingerprintOneFMDTemplate, mFingerprintTwoFMDTemplate);
+		});
+	}
+
+	// --------------------------------------------------------------------------------------------
+	//
+	// Methods related to UI control.
+	//
+	// --------------------------------------------------------------------------------------------
+	@SuppressWarnings("SameParameterValue")
+	private void
+	setCaptureMatchButtonEnable(boolean enable) {
+		mCaptureButton.setEnabled(enable);
+		mMatchButton.setEnabled(enable);
+	}
+
+	@SuppressWarnings("SameParameterValue")
+	private void
+	setAllButtonEnable(boolean enable) {
+		mOpenCloseButton.setEnabled(enable);
+		this.setCaptureMatchButtonEnable(enable);
+	}
+
+	// --------------------------------------------------------------------------------------------
+	//
+	// Methods used for performing some type of operation/processing.
+	//
+	// --------------------------------------------------------------------------------------------
+	private void
+	captureFingerprintOne() {
+		mFingerprintOneFMDTemplate = null;
+
+		// OnFingerprintGrabbedWSQListener: This listener is to be used if you wish to obtain a WSQ
+		// template, fingerprint quality score, along with captured fingerprint image. This saves
+		// from having to make separate API calls.
+		mBiometricsManager.grabFingerprint(mScanTypes[0], new OnFingerprintGrabbedWSQListener() {
+			@SuppressLint("SetTextI18n")
+			@Override
+			public void onFingerprintGrabbed(ResultCode resultCode,
+											 Bitmap bitmap,
+											 byte[] bytes,
+											 String filepath,
+											 String wsqFilepath,
+											 String hint,
+											 int nfiqScore) {
+				// If a valid hint was given then display it for user to see.
+				if (hint != null && !hint.isEmpty())
+					mStatusTextView.setText(hint);
+
+				// On each iteration of this callback we get a Bitmap of actual fingerprint.
+				if (bitmap != null)
+					mFingerprintOneImageView.setImageBitmap(bitmap);
+
+				// This case may occur if "cancelCapture()" or "closeFingerprint()" are called while
+				// in middle of capture.
+				if (resultCode == INTERMEDIATE && hint != null && hint.equals("Capture Stopped")) {
+					mOpenCloseButton.setEnabled(true);
+					mCaptureButton.setEnabled(true);
+				}
+
+				// This ResultCode is returned once fingerprint has been fully captured.
+				if (resultCode == OK) {
+					mInfoTextView.setText("Quality: " + nfiqScore);
+					mCaptureButton.setEnabled(true);
+					mOpenCloseButton.setEnabled(true);
+
+					createFMDTemplate(bitmap);
+				}
+
+				if (resultCode == FAIL) {
+					mOpenCloseButton.setEnabled(true);
+					mCaptureButton.setEnabled(true);
+				}
+			}
+
+			@Override
+			public void onCloseFingerprintReader(ResultCode resultCode,
+												 CloseReasonCode closeReasonCode) {
+
+			}
+		});
+	}
+
+	private void
+	captureFingerprintTwo() {
+		mFingerprintTwoFMDTemplate = null;
+
+		mBiometricsManager.grabFingerprint(mScanTypes[0], new OnFingerprintGrabbedListener() {
+			@Override
+			public void onFingerprintGrabbed(ResultCode resultCode,
+											 Bitmap bitmap,
+											 byte[] bytes,
+											 String s,
+											 String hint) {
+
+				// If a valid hint was given then display it for user to see.
+				if (hint != null && !hint.isEmpty())
+					mStatusTextView.setText(hint);
+
+				// On each iteration of this callback we get a Bitmap of actual fingerprint.
+				if (bitmap != null)
+					mFingerprintTwoImageView.setImageBitmap(bitmap);
+
+				// This case may occur if "cancelCapture()" or "closeFingerprint()" are called while
+				// in middle of capture.
+				if (resultCode == INTERMEDIATE && hint != null && hint.equals("Capture Stopped")) {
+					mOpenCloseButton.setEnabled(true);
+					mCaptureButton.setEnabled(true);
+				}
+
+				// This ResultCode is returned once fingerprint has been fully captured.
+				if (resultCode == OK) {
+					mCaptureButton.setEnabled(true);
+					mOpenCloseButton.setEnabled(true);
+
+					createFMDTemplate(bitmap);
+				}
+
+				if (resultCode == FAIL) {
+					mOpenCloseButton.setEnabled(true);
+					mCaptureButton.setEnabled(true);
+				}
+			}
+
+			@Override
+			public void onCloseFingerprintReader(ResultCode resultCode,
+												 CloseReasonCode closeReasonCode) {
+
+			}
+		});
+	}
+
+	@SuppressLint("SetTextI18n")
+	private void
+	createFMDTemplate(Bitmap bitmap) {
+		final long startTime = SystemClock.elapsedRealtime();
+
+		mBiometricsManager.convertToFmd(bitmap, ISO_19794_2_2005, (ResultCode resultCode,
+																   byte[] bytes) -> {
+			if (resultCode != OK) {
+				mStatusTextView.setText("Failed to create FMD template.");
+				return;
+			}
+
+			final double durationInSeconds = (SystemClock.elapsedRealtime() - startTime) / 1000.0;
+			mInfoTextView.setText("Created FMD template in: " + durationInSeconds + " seconds.");
+
+			if (mCaptureFingerprintOne)
+				mFingerprintOneFMDTemplate = bytes.clone();
+			else mFingerprintTwoFMDTemplate = bytes.clone();
+
+			if (mFingerprintOneFMDTemplate != null && mFingerprintTwoFMDTemplate != null)
+				mMatchButton.setEnabled(true);
+		});
+	}
+
+	@SuppressLint("SetTextI18n")
+	private void
+	matchFMDTemplates(byte[] templateOne,
+					  byte[] templateTwo) {
+		mBiometricsManager.compareFmd(templateOne, templateTwo, ISO_19794_2_2005,
+				(ResultCode resultCode, float dissimilarity) -> {
+					this.setAllButtonEnable(true);
+
+					if (resultCode != OK) {
+						mStatusTextView.setText("Failed to compare templates.");
+						mInfoTextView.setText("");
+						return;
+					}
+
+					String matchDecision = "No Match";
+					if (dissimilarity < (Integer.MAX_VALUE / 1000000))
+						matchDecision = "Match";
+
+					mStatusTextView.setText("Matching complete.");
+					mInfoTextView.setText("Match outcome: " + matchDecision);
+				});
+	}
+}
