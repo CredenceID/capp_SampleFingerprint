0a1171c78fe07de4187ef84bf4a2b92172a0dea8
Avi Varshney <avarshney65@gmail.com>
2019-04-25 13:46:13 -0700

    Updated sample application.

--- app/src/main/java/com/credenceid/sample/fingerprint/LaunchActivity.java
+++ app/src/main/java/com/credenceid/sample/fingerprint/LaunchActivity.java
@@ -13,94 +13,98 @@ import com.credenceid.biometrics.DeviceType;
 import static android.widget.Toast.LENGTH_LONG;
 import static android.widget.Toast.LENGTH_SHORT;
 import static com.credenceid.biometrics.Biometrics.ResultCode.FAIL;
+import static com.credenceid.biometrics.Biometrics.ResultCode.INTERMEDIATE;
 import static com.credenceid.biometrics.Biometrics.ResultCode.OK;
 
-@SuppressWarnings({"unused", "StaticFieldLeak"})
+@SuppressWarnings({"unused", "StaticFieldLeak", "StatementWithEmptyBody"})
 public class LaunchActivity
-		extends Activity {
+        extends Activity {
 
-	private static final String TAG = LaunchActivity.class.getSimpleName();
+    private static final String TAG = LaunchActivity.class.getSimpleName();
 
-	/* CredenceSDK biometrics object, used to interface with APIs. */
-	private static BiometricsManager mBiometricsManager;
-	/* Stores which Credence family of device's this app is running on. */
-	private static DeviceFamily mDeviceFamily = DeviceFamily.InvalidDevice;
-	/* Stores which specific device this app is running on. */
-	private static DeviceType mDeviceType = DeviceType.InvalidDevice;
+    /* CredenceSDK biometrics object, used to interface with APIs. */
+    private static BiometricsManager mBiometricsManager;
+    /* Stores which Credence family of device's this app is running on. */
+    private static DeviceFamily mDeviceFamily = DeviceFamily.InvalidDevice;
+    /* Stores which specific device this app is running on. */
+    private static DeviceType mDeviceType = DeviceType.InvalidDevice;
 
-	/* --------------------------------------------------------------------------------------------
-	 *
-	 * Public getter methods.
-	 *
-	 * --------------------------------------------------------------------------------------------
-	 */
+    /* --------------------------------------------------------------------------------------------
+     *
+     * Public getter methods.
+     *
+     * --------------------------------------------------------------------------------------------
+     */
 
-	public static BiometricsManager
-	getBiometricsManager() {
+    public static BiometricsManager
+    getBiometricsManager() {
 
-		return mBiometricsManager;
-	}
+        return mBiometricsManager;
+    }
 
-	public static DeviceType
-	getDeviceType() {
+    public static DeviceType
+    getDeviceType() {
 
-		return mDeviceType;
-	}
+        return mDeviceType;
+    }
 
-	public static DeviceFamily
-	getDeviceFamily() {
+    public static DeviceFamily
+    getDeviceFamily() {
 
-		return mDeviceFamily;
-	}
+        return mDeviceFamily;
+    }
 
-	/* --------------------------------------------------------------------------------------------
-	 *
-	 * Android activity lifecycle event methods.
-	 *
-	 * --------------------------------------------------------------------------------------------
-	 */
+    /* --------------------------------------------------------------------------------------------
+     *
+     * Android activity lifecycle event methods.
+     *
+     * --------------------------------------------------------------------------------------------
+     */
 
-	@Override
-	protected void
-	onCreate(Bundle savedInstanceState) {
+    @Override
+    protected void
+    onCreate(Bundle savedInstanceState) {
 
-		super.onCreate(savedInstanceState);
-		this.initBiometrics();
-	}
+        super.onCreate(savedInstanceState);
+        this.initBiometrics();
+    }
 
-	/* --------------------------------------------------------------------------------------------
-	 *
-	 * Private helpers.
-	 *
-	 * --------------------------------------------------------------------------------------------
-	 */
+    /* --------------------------------------------------------------------------------------------
+     *
+     * Private helpers.
+     *
+     * --------------------------------------------------------------------------------------------
+     */
 
-	private void
-	initBiometrics() {
+    private void
+    initBiometrics() {
 
-		/*  Create new biometrics object. */
-		mBiometricsManager = new BiometricsManager(this);
+        /*  Create new biometrics object. */
+        mBiometricsManager = new BiometricsManager(this);
 
-		/* Initialize object, meaning tell CredenceService to bind to this application. */
-		mBiometricsManager.initializeBiometrics((Biometrics.ResultCode resultCode,
-												 String minimumVersion,
-												 String currentVersion) -> {
+        /* Initialize object, meaning tell CredenceService to bind to this application. */
+        mBiometricsManager.initializeBiometrics((Biometrics.ResultCode resultCode,
+                                                 String minimumVersion,
+                                                 String currentVersion) -> {
 
-			/* This API will never return ResultCode.INTERMEDIATE. */
+            if (OK == resultCode) {
+                Toast.makeText(this, getString(R.string.biometrics_initialized), LENGTH_SHORT).show();
 
-			if (OK == resultCode) {
-				Toast.makeText(this, getString(R.string.biometrics_initialized), LENGTH_SHORT).show();
+                mDeviceFamily = mBiometricsManager.getDeviceFamily();
+                mDeviceType = mBiometricsManager.getDeviceType();
 
-				mDeviceFamily = mBiometricsManager.getDeviceFamily();
-				mDeviceType = mBiometricsManager.getDeviceType();
+                /* Launch main activity. */
+                Intent intent = new Intent(this, FingerprintActivity.class);
+                intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+                startActivity(intent);
+                this.finish();
 
-				/* Launch main activity. */
-				Intent intent = new Intent(this, FingerprintActivity.class);
-				intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
-				startActivity(intent);
-				this.finish();
-			} else if (FAIL == resultCode)
-				Toast.makeText(this, getString(R.string.biometrics_fail_init), LENGTH_LONG).show();
-		});
-	}
+            } else if (INTERMEDIATE == resultCode) {
+                /* This code is never returned here. */
+
+            } else if (FAIL == resultCode) {
+                Toast.makeText(this, getString(R.string.biometrics_fail_init), LENGTH_LONG).show();
+            }
+        });
+    }
 }
